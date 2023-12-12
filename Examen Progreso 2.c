dc0b5db32d3791b6dbb83b6288052bd9bea27707
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-12-12 15:21:01 -0500

    Llamo a mis funciones al codigo y imprimo los insumos

--- Examen Progreso 2.c
+++ Examen Progreso 2.c
@@ -8,8 +8,8 @@ void asignarValoresinsumos(int insumos[]) {
         printf("Insumo N%d: %d\n ",i, insumos[i]);
     }
     printf("\n");
-// con un funcion doy valores aleatorios a mi insumos de ingresos
-
+    //doy valores de aleatorios a mis insumos
+    
 }
 void asignarValoresegresos(int insumos[]) {
     printf("Numero de egresos de insumos : \n");
@@ -18,9 +18,69 @@ void asignarValoresegresos(int insumos[]) {
         printf("Insumo N%d: %d\n ",i, insumos[i]);
     }
     printf("\n");
-    // con un funcion doy valores aleatorios a mi insumos de egresos
 }
 
-
-
 int main (){
+srand (time(NULL));
+printf ("Ingresos y egresos mes de Enero\n");
+int insumos[30];
+asignarValoresinsumos(insumos);
+asignarValoresegresos(insumos);
+//llamo a mi funcion para que me entregue los valores de ingresos y egresos 
+
+printf ("Ingresos y egresos mes de Febrero\n");
+int febrero[30];
+asignarValoresinsumos(febrero);
+asignarValoresegresos(febrero);
+// repito los mismo para mis meses del año
+printf ("Ingresos y egresos mes de Marzo\n");
+int marzo[30];
+asignarValoresinsumos(marzo);
+asignarValoresegresos(marzo);
+
+printf ("Ingresos y egresos mes de Abri\n");
+int abril[30];
+asignarValoresinsumos(abril);
+asignarValoresegresos(abril);
+
+printf ("Ingresos y egresos mes de Mayo\n");
+int mayo[30];
+asignarValoresinsumos(mayo);
+asignarValoresegresos(mayo);
+
+printf ("Ingresos y egresos mes de Junio\n");
+int junio[30];
+asignarValoresinsumos(junio);
+asignarValoresegresos(junio);
+
+printf ("Ingresos y egresos mes de Julio\n");
+int Julio[30];
+asignarValoresinsumos(Julio);
+asignarValoresegresos(Julio);
+
+printf ("Ingresos y egresos mes de Agosto\n");
+int agos[30];
+asignarValoresinsumos(agos);
+asignarValoresegresos(agos);
+
+printf ("Ingresos y egresos mes de Septiembre\n");
+int sep[30];
+asignarValoresinsumos(sep);
+asignarValoresegresos(sep);
+
+printf ("Ingresos y egresos mes de Octubre\n");
+int oc[30];
+asignarValoresinsumos(oc);
+asignarValoresegresos(oc);
+
+printf ("Ingresos y egresos mes de Noviembre\n");
+int nov[30];
+asignarValoresinsumos(nov);
+asignarValoresegresos(nov);
+
+printf ("Ingresos y egresos mes de Diciembre\n");
+int dis[30];
+asignarValoresinsumos(dis);
+asignarValoresegresos(dis);
+return 0;
+}
